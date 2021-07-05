10491c6381e6eeeb2815ff435359de2ca09e241b
Xoma <xromenkovevgenii@mail.ru>
2021-07-05 22:12:39 +0300

    Реализован цикл ввода данных по просмотренным фильмама и прописаны условия ввода информации о количестве просмотренных фильмов

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-"use strict";
+ "use strict";
 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
-const personalMovieDb = {
-        count: numberOfFilms,
-        movies: {
-
-        },
-        actors: {
-
-        },
-        genres: [
-
-        ],
-        private: false,
-    };
-
-const a = prompt("Один из последних просмотренных фильмов?", " "),
-      b = prompt("На сколько оцените его?", " "),
-      c = prompt("Один из последних просмотренных фильмов?", " "),
-      d = prompt("На сколько оцените его?", " ");
+ const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
 
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+ const personalMovieDb = {
+     count: numberOfFilms,
+     movies: {
+
+     },
+     actors: {
+
+     },
+     genres: [
+
+     ],
+     private: false,
+ };
+
+  for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+      if (a != null && b != null && a != '' && b != '' & a.length < 50) {
+          personalMovieDb.movies[a] = b;
+          console.log('done');
+      } else {
+          console.log('error');
+          i--;
+      }
+  }
+  
+ const a = prompt("Один из последних просмотренных фильмов?", ""),
+     b = prompt("На сколько оцените его?", "");
+     
+
+ if (personalMovieDb.count < 10) {
+     console.log("Просмотрено довольно мало фильмов");
+ } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+     console.log("Вы классический зритель");
+ } else if (personalMovieDb.count >= 30) {
+     console.log("Вы киноман");
+ } else {
+     console.log('Произошла ошибка');
+ }
 
 
-console.log(personalMovieDb);
 
+
+
+
+ //  console.log(personalMovieDb);
